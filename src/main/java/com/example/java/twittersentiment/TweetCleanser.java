4a474dcee8079296c7b8fc611d8dae2d631f9931
Shubham0411 <shubhu8800@gmail.com>
2017-06-14 11:50:47 +0530

    Addition of POJO and  using it to send to next Operator

--- src/main/java/com/example/java/twittersentiment/TweetCleanser.java
+++ src/main/java/com/example/java/twittersentiment/TweetCleanser.java
@@ -6,7 +6,6 @@ urls from the tweet and then forwards both
 original (for reference)
 as well as cleaned tweet for further analysis.
  */
-import java.util.SortedMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,65 +23,55 @@ import com.datatorrent.common.util.BaseOperator;
 
 public class TweetCleanser extends BaseOperator
 {
+  Data data = new Data();
   public static final Logger LOG = LoggerFactory.getLogger(TweetCleanser.class);
 
-  public static String getFILENAME()
-  {
-    return FILENAME;
-  }
 
-  public static void setFILENAME(String FILENAME)
-  {
-    TweetCleanser.FILENAME = FILENAME;
-  }
-
-  protected static String FILENAME ;
-
-  public final transient DefaultOutputPort<String> cleanedtweet = new DefaultOutputPort<>();
-  public final transient DefaultOutputPort<String> originaltweet = new DefaultOutputPort<>();
-  public SortedMap<String, Integer> afinnSentimentMap = null;
-  public TweetCleanser()
-  {
-
-  }
-
-
-  public final transient DefaultInputPort<String> originaltweet1 = new DefaultInputPort<String>()
+  public final transient DefaultOutputPort<Data> output = new DefaultOutputPort<>();
+  public final transient DefaultInputPort<String> originalTweetInputAtCleanser = new DefaultInputPort<String>()
   {
     @Override
-    public void process(String receivedtweet)
+    public void process(String receivedTweet)
     {
-        LOG.info("Current tweet" + receivedtweet);
-        originaltweet.emit(receivedtweet);
-        //the original tweet is passed to the function to remove the url
-        removeUrl(receivedtweet);
+      LOG.info("Current tweet" + receivedTweet);
+      data.originalTweetInCleanser = receivedTweet;
+
+      // it will first call removeUrl than puntuationRemover.
+      String urlRemovedTweet = removeUrl(receivedTweet);
+      punctuationRemover(urlRemovedTweet);
     }
   };
 
   // will remove all the punctuations from a tweet
-  public void punctuationRemover(String curr_tweet)
+  public Data punctuationRemover(String currTweet)
   {
-
     //Remove all punctuation and new line chars in the tweet and converting into lowercase.
-    String tweet = curr_tweet.replaceAll("\\p{Punct}|\\n", " ").toLowerCase();
+    String tweet = currTweet.replaceAll("\\p{Punct}|\\n", " ").toLowerCase();
     LOG.info("cleaned tweet  is --> " +tweet);
-    cleanedtweet.emit(tweet);
 
+    data.cleanedTweetInCleanser = tweet;
+    output.emit(data);
+    return data;
   }
 
 
-  public void removeUrl(String filteredtweet)
+  public String removeUrl(String filteredTweet)
   {
     String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
     Pattern p = Pattern.compile(urlPattern,Pattern.CASE_INSENSITIVE);
-    Matcher m = p.matcher(filteredtweet);
+    Matcher m = p.matcher(filteredTweet);
     int i = 0;
     while (m.find()) {
-      filteredtweet = filteredtweet.replaceAll(m.group(i),"").trim();
+      filteredTweet = filteredTweet.replaceAll(m.group(i),"").trim();
       i++;
     }
-    LOG.info("cleaned from url tweet  is --> " +filteredtweet);
-    // passes the cleaned tweet from here to the given function
-    punctuationRemover(filteredtweet);
+    LOG.info("cleaned from url tweet  is --> " +filteredTweet);
+    return filteredTweet;
   }
+}
+
+class Data
+{
+  String cleanedTweetInCleanser;
+  String originalTweetInCleanser;
 }
